69f0b3af8ad562f05f890ca78fc880500f317a45
GovindPothuraju <govindpothuraju2006@gmail.com>
2025-06-24 12:04:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Pair Sum in a Sorted and Rotated Array/pair-sum-in-a-sorted-and-rotated-array.java
+++ Difficulty: Medium/Pair Sum in a Sorted and Rotated Array/pair-sum-in-a-sorted-and-rotated-array.java
@@ -2,7 +2,7 @@
 class Solution {
     static boolean pairInSortedRotated(int arr[], int target) {
         // Your code here
-        HashMap<Integer,Integer> hm=new HashMap<>();
+        /*HashMap<Integer,Integer> hm=new HashMap<>();
         int n=arr.length;
         for(int i=0;i<n;i++){
             if(arr[i]>target){
@@ -14,6 +14,45 @@ class Solution {
             }
             hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
         }
+        return false;*/
+        /*Arrays.sort(arr);
+        int n=arr.length;
+        int l=0;
+        int r=n-1;
+        while(l<r){
+            if(arr[l]+arr[r]==target){
+                return true;
+            }
+            if(arr[l]+arr[r]>target){
+                r--;
+            }else{
+                l++;
+            }
+        }
+        return false;*/
+        // without sorting
+        int n=arr.length;
+        int bp=0;
+        for(int i=1;i<n;i++){
+            if(arr[i-1]>arr[i]){
+                bp=i;
+                break;
+            }
+        }
+        int l=bp;
+        int r=(n+bp-1)%n;
+        while(l!=r){
+            if(arr[l]+arr[r]==target){
+                return true;
+            }
+            else if(arr[l]+arr[r]>target){
+                r=(n+r-1)%n;
+            }else{
+                l=(l+1)%n;
+            }
+        }
         return false;
+   
+        
     }
 }
