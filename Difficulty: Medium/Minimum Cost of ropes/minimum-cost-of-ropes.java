d835c4f350c9f950d0ffe49484fc1fe7ef1fd004
Govindha Pothuraju <govindpothuraju2006@gmail.com>
2026-03-02 12:17:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Cost of ropes/minimum-cost-of-ropes.java
+++ Difficulty: Medium/Minimum Cost of ropes/minimum-cost-of-ropes.java
@@ -1,19 +1,19 @@
 class Solution {
     public static int minCost(int[] arr) {
         // code here
-        PriorityQueue<Integer> q = new PriorityQueue<>();
-        for (int num : arr) {
-            q.offer(num);
+        if(arr.length == 1) return 0;
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int i=0;i<arr.length;i++){
+            pq.add(arr[i]);
         }
         int res=0;
-        while (!q.isEmpty()) {
-            int x=q.poll();
-            if(q.isEmpty()){
-                return res;
-            }
-            int y=q.poll();
-            res+=(x+y);
-            q.offer(x+y);
+        while(!pq.isEmpty()){
+            int v1=0,v2=0;
+            if(!pq.isEmpty())v1=pq.poll();
+            if(!pq.isEmpty())v2=pq.poll();
+            int temp=v1+v2;
+            res+=temp;
+            if(!pq.isEmpty())pq.add(temp);
         }
         return res;
     }
